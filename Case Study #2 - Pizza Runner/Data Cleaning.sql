03c0f4d7bfb2a181221c8e15bcf1706135fc0bad
Millicent <94468487+MillicentAbban@users.noreply.github.com>
2022-10-19 00:36:20 +0200

    Update Data Cleaning.sql

--- Case Study #2 - Pizza Runner/Data Cleaning.sql
+++ Case Study #2 - Pizza Runner/Data Cleaning.sql
@@ -1,4 +1,27 @@
 # Pizza Runner
 ##TABLE : Customer Orders
-having a view of the customer orders table reveal some issues that need to be corrected.
+Having a view of the customer orders table reveal some issues that need to be corrected.
 - The exclusions and extra column will need to be cleaned of the nulls 
+
+
+```sql
+Select order_id,
+	   customer_id,
+	   pizza_id,
+CASE 
+	WHEN exclusions LIKE 'NULL' or exclusions LIKE 'NULLL' THEN ''
+	ELSE exclusions
+	END AS exclusions,
+CASE 
+	WHEN extras LIKE 'NULL' or extras LIKE 'NULL' THEN ''
+	ELSE extras
+	END AS extras,
+	order_time
+INTO Customer_orders_temp
+
+From Customer_orders
+
+Select *
+From Customer_orders_temp;
+
+```
